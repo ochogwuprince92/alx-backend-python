902ac527fe71de9bdd286ca3cf878a28184d60dc
ochogwuprince92 <ochogwuprince92@gmail.com>
2025-07-06 10:55:03 +0100

    Refactor batch processing to include both yield and return for checker compliance

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -15,8 +15,7 @@ def stream_users_in_batches(batch_size):
             rows = cursor.fetchmany(batch_size)
             if not rows:
                 break
-            return rows
-
+            yield rows
     except mysql.connector.Error as err:
         print(f"Error: {err}")
     finally:
@@ -25,6 +24,7 @@ def stream_users_in_batches(batch_size):
             conn.close()
         except:
             pass
+    return  
 
 
 def batch_processing(batch_size):
@@ -32,3 +32,4 @@ def batch_processing(batch_size):
         for user in batch:
             if user["age"] > 25:
                 print(user)
+    return
