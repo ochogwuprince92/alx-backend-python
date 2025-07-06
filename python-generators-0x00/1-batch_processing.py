eb42dde1e4c58177ba0df4ffa5eaeca17095526a
ochogwuprince92 <ochogwuprince92@gmail.com>
2025-07-06 10:48:28 +0100

    Implement batch processing of user data using generators with filtering

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='devuser',
+            password='Devpass123!',
+            database='ALX_prodev'
+        )
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+
+        while True:
+            rows = cursor.fetchmany(batch_size)
+            if not rows:
+                break
+            yield rows
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        try:
+            cursor.close()
+            conn.close()
+        except:
+            pass
+
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user["age"] > 25:
+                print(user)
