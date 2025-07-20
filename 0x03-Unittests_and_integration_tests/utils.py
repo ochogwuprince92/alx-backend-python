3d63d263a7c190bbdd314e043ed51432e3e4ec33
ochogwuprince92 <ochogwuprince92@gmail.com>
2025-07-20 07:33:05 +0100

    feat(tests): Add parameterized unit tests for access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Utils module"""
+
+from typing import Mapping, Any, Sequence
+
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access nested maps using a sequence of keys"""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
