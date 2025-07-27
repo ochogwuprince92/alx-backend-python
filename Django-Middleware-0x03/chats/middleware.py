732bab5835a524f9167b85fb5bef4db6cce03466
ochogwuprince92 <ochogwuprince92@gmail.com>
2025-07-27 17:29:42 +0100

    Updated requestlog

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,19 +1,17 @@
-import logging
+# chats/middleware.py
+
 from datetime import datetime
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        logging.basicConfig(
-            filename='Django-Middleware-0x03/chats/requests.log',
-            level=logging.INFO,
-            format='%(message)s'
-        )
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else 'Anonymous'
-        log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        logging.info(log_entry)
+        log_line = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
+
+        with open("chats/requests.log", "a") as log_file:
+            log_file.write(log_line)
 
         response = self.get_response(request)
         return response
